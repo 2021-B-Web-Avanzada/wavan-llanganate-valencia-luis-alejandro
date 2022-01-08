dc1b9ec1ffd5a228128b29eb9845a42f2a053930
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2022-01-08 12:29:03 -0500

    feat: add material UI dependencies

--- PRIMER BIMESTRE/EXAM/github-clone/src/app/app.module.ts
+++ PRIMER BIMESTRE/EXAM/github-clone/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Material
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
