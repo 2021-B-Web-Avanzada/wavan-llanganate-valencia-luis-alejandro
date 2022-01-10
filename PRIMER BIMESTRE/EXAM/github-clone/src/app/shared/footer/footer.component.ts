df1f939d0ffff70dcb7eab197b6c3c0875b68030
Alejandro Llanganate <luis.llanganate@epn.edu.ec>
2022-01-09 21:58:01 -0500

    feat: add footer

--- PRIMER BIMESTRE/EXAM/github-clone/src/app/shared/footer/footer.component.ts
+++ PRIMER BIMESTRE/EXAM/github-clone/src/app/shared/footer/footer.component.ts
@@ -7,6 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  options = [
+    {
+      name: "Terms",
+      link: "https://docs.github.com/en/github/site-policy/github-terms-of-service",
+    },
+    {
+      name: "Privacy",
+      link: "https://docs.github.com/en/github/site-policy/github-privacy-statement",
+    },
+    {
+      name: "Security",
+      link: "https://github.com/security",
+    },
+    {
+      name: "Status",
+      link: "https://www.githubstatus.com/",
+    },
+    {
+      name: "Docs",
+      link: "https://docs.github.com/",
+    },
+    {
+      name: "Contact GitHub",
+      link: "https://support.github.com/?tags=dotcom-footer",
+    },
+    {
+      name: "Pricing",
+      link: "https://github.com/pricing",
+    },
+    {
+      name: "API",
+      link: "https://docs.github.com/",
+    },
+    {
+      name: "Training",
+      link: "https://services.github.com/",
+    },
+    {
+      name: "Blog",
+      link: "https://github.blog/",
+    },
+    {
+      name: "About",
+      link: "https://github.com/about"
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
